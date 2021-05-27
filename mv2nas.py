8047542960a127b7f7d0f913f2b31e7b540a919a
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-05-27 08:52:20 +0200

    Update mv2nas.py

--- mv2nas.py
+++ mv2nas.py
@@ -21,6 +21,7 @@ search_strings_and_target_folders = [["Bibi Blocksberg", "TVSendungen_Kinder/Bib
                                      ["Löwenzahn", "TVSendungen_Kinder/Löwenzahn"],
                                      ["Meine Freundin Conni", "TVSendungen_Kinder/Conni"],
                                      ["Lieselotte", "TVSendungen_Kinder/Lieselotte"],
+                                     ["Lassie", "TVSendungen_Kinder/Lassie"],
                                      ["Mia and me", "TVSendungen_Kinder/Mia_and_me"],
                                      ["Mascha und der Bär", "TVSendungen_Kinder/Mascha_und_der_Bär"],
                                      ["Mama Fuchs und Papa Dachs", "TVSendungen_Kinder/Mama_Fuchs_und_Papa_Dachs"],
