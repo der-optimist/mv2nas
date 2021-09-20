28a789e894a4ef04586978fb68f6ba8f9f7e16ef
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-09-20 16:55:06 +0200

    Update mv2nas.py

--- mv2nas.py
+++ mv2nas.py
@@ -69,6 +69,7 @@ search_strings_and_target_folders = [["Bibi Blocksberg", "TVSendungen_Kinder/Bib
                                      ["Krimis im Ersten - Der Masuren-Krimi", "TVSendungen/90_-_KiE_Masuren-Krimi"],
                                      ["Krimis im Ersten - Der Lissabon-Krimi", "TVSendungen/90_-_KiE_Lissabon-Krimi"],
                                      ["Krimis im Ersten - Der Amsterdam-Krimi", "TVSendungen/90_-_KiE_Amsterdam-Krimi"],
+                                     ["Krimis im Ersten*Der Usedom-Krimi", "TVSendungen/90_-_KiE_Usedom-Krimi"],
                                      ["Krimis im Ersten - Wolfsland", "TVSendungen/90_-_KiE_Wolfsland"],
                                      ["Krimis im Ersten - Kommissar Dupin", "TVSendungen/90_-_KiE_Kommissar_Dupin"],
                                      ["Krimis im Ersten - Der Zürich-Krimi", "TVSendungen/90_-_KiE_Zürich-Krimi"],
