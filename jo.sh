40d75f081fe33ba902754b44bfadf748bba98452
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-12-04 21:22:29 +0100

    Delete jo.sh

--- jo.sh
+++
@@ -1,8 +0,0 @@
-rename 's/(^.{10}_[^_]+_)(.+)(\s\-\s)\2(.+)/$1$2$4/' *
-mv *heute-show* /srv/dev-disk-by-label-ssddata/ssddata/jo
-mv *Wismar* /srv/dev-disk-by-label-ssddata/ssddata/jo
-mv *Anstalt* /srv/dev-disk-by-label-ssddata/ssddata/jo
-mv *Frontal* /srv/dev-disk-by-label-ssddata/ssddata/jo
-mv *ROYALE* /srv/dev-disk-by-label-ssddata/ssddata/jo
-mv *History* /srv/dev-disk-by-label-ssddata/ssddata/jo
-mv *Doku* /srv/dev-disk-by-label-ssddata/ssddata/jo
